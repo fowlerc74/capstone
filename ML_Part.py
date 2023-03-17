4bae3c85c7b20c3af347ef114069c1c6434d9097
Cade Fowler <bcfowler2@catamount.wcu.edu>
2023-03-17 02:23:48 -0400

    wrote pca and gaussian

--- ML_Part.py
+++ ML_Part.py
@@ -1,16 +1,18 @@
 from pyspark.ml.feature import StringIndexer, VectorAssembler, StandardScaler
 from pyspark.ml.regression import LinearRegression
-from pyspark.ml.clustering import KMeans
+from pyspark.ml.clustering import KMeans, LDA
 from pyspark.ml.evaluation import ClusteringEvaluator
 from pyspark.mllib.classification import NaiveBayes
 from pyspark.mllib.linalg import Vectors
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.sql.functions import col
 import matplotlib.pyplot as plt
-
-
-from pyspark.mllib.clustering import GaussianMixture
+from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
+from pyspark.mllib.util import MLUtils
+from pyspark.ml.feature import PCA
+from pyspark.ml.clustering import GaussianMixture
 from numpy import array
+
 
 
 ####################### Linear Regression #########################
@@ -158,14 +160,10 @@ def kmeans(sdf):
     ax.set_ylabel('weather')
     fig.savefig('kmeans.png')
 
-########## Naive Bayes ##########
-# 
-def naive_bayes(sdf):
-    # def parseLine(line):
-    #     parts = line.split(',')
-    #     label = float(parts[0])
-    #     features = Vectors.dense([float(x) for x in parts[1].split(' ')])
-    #     return LabeledPoint(label, features)
+####### Gaussian Mixture #######
+# Clustering Algorithm
+def gaussian(sdf):
+    num_k = 4
     assembler = VectorAssembler(inputCols=['DailyAverageDryBulbTemperature',
                                         'DailyAverageRelativeHumidity',
                                         'DailyAverageSeaLevelPressure',
@@ -180,41 +178,67 @@ def naive_bayes(sdf):
                                         'DailyPeakWindDirection',
                                         'DailyPeakWindSpeed',
                                         'DailySustainedWindSpeed',
-                                        'DailyWeather'], outputCol= 'Features')
-    transformed = assembler.transform(sdf)
-    data = (transformed.select(col("outcome_column").alias("label"), col("features")).rdd \
-        .map(lambda row: LabeledPoint(row.label, row.features)))
-    print(type(data))
-
-    # Split data aproximately into training (60%) and test (40%)
-    training, test = data.randomSplit([0.6, 0.4], seed = 0)
-
-    # Train a naive Bayes model.
-    model = NaiveBayes.train(training, 1.0)
-
-    # Make prediction and test accuracy.
-    predictionAndLabel = test.map(lambda p : (model.predict(p.features), p.label))
-    accuracy = 1.0 * predictionAndLabel.filter(lambda x, v: x == v).count() / test.count()
-    print(accuracy)
-
-####### Gaussian Mixture #######
-#
-def gaussian(sdf):
-    # parse the data
-    parsedData = sdf.rdd.map(lambda line: array([float(x) for x in line.strip().split(' ')]))
+                                        'DailyWeather'], outputCol= 'features')
+    df = assembler.transform(sdf)
+    #parsedData = df.map(lambda line: array([float(x) for x in line.strip().split(' ')]))
 
     # Build the model (cluster the data)
-    gmm = GaussianMixture.train(parsedData, 2)
+    gm = GaussianMixture(k=num_k, tol=.001)
+    gm.setMaxIter(30)
+    model = gm.fit(df)
 
+    
     # output parameters of model
-    for i in range(2):
-        print ("weight = ", gmm.weights[i], "mu = ", gmm.gaussians[i].mu,
-            "sigma = ", gmm.gaussians[i].sigma.toArray())
+    print("\n\nGaussian Mixture Model (GMM)")
+    print("============================")
+    print("Number of clusters: ", num_k)
+    print("Max iterations: ", gm.getMaxIter())
+    # print("Number of Features: ", len(model.getFeaturesCol()), "\n")
+    for i in range(num_k):
+        print("--------------------------------------")
+        print("Cluster ", str(i + 1), ":")
+        print("# of items: ", model.summary.clusterSizes[i])
+        print("Weight: ", model.weights[i])
+        print("--------------------------------------")
+        
+####### Principal Component Analysis #######
+# PCA: Reduces dimensionality of large data sets
+def pca(sdf):
+    # Change this so that each one is its own vector maybe ?
+    assembler = VectorAssembler(inputCols=['DailyAverageDryBulbTemperature',
+                                        'DailyAverageRelativeHumidity',
+                                        'DailyAverageSeaLevelPressure',
+                                        'DailyAverageStationPressure',
+                                        'DailyAverageWetBulbTemperature',
+                                        'DailyAverageWindSpeed',
+                                        'DailyCoolingDegreeDays',
+                                        'DailyDepartureFromNormalAverageTemperature',
+                                        'DailyHeatingDegreeDays',
+                                        'DailyMaximumDryBulbTemperature',
+                                        'DailyMinimumDryBulbTemperature',
+                                        'DailyPeakWindDirection',
+                                        'DailyPeakWindSpeed',
+                                        'DailySustainedWindSpeed',
+                                        'DailyWeather'], outputCol= 'features')
+    df = assembler.transform(sdf)
 
+    # setup pca
+    pca = PCA(k=2, inputCol="features")
+    pca.setOutputCol("PCA_Features")
+    # run pca
+    model = pca.fit(df)
+    model.setOutputCol("output")
+    
+    # output
+    print("Principal Component Analysis")
+    print("============================")
+    for out in model.transform(df).collect():
+        print(out.output)
+    
 
 # Machine learning display options for which algorithm to use.
 def machine_menu(sdf):
-    display = "\n1) Linear Regression\n2) K-Means\n3) Naive Bayes\n4) Gaussian Mixture\n0) Exit"
+    display = "\n1) Linear Regression\n2) K-Means\n3) Gaussian Mixture\n4) Principal Component Analysis (PCA)\n0) Exit"
     print(display)
     # Pick option
     user_input = input("Pick a option: ")
@@ -227,9 +251,9 @@ def machine_work(ui,sdf):
     elif ui == "2":
         kmeans(sdf)
     elif ui == "3":
-        naive_bayes(sdf)
-    elif ui == "4":
         gaussian(sdf)
+    elif ui == "4":
+        pca(sdf)
     elif ui == "0":
         return
     else:
