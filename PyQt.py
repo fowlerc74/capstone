d8e9e6036eca1ef791f406f59aee76a18866496e
zalane2 <72633029+zalane2@users.noreply.github.com>
2023-03-30 21:45:27 -0400

    Added QLineEdit Example

--- PyQt.py
+++ PyQt.py
@@ -1,6 +1,69 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QLineEdit
 from PyQt6.QtCore import Qt
 import sys
+
+# How to change current displayed text in QLabel Widget
+# class Window(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#         # Window dimensions
+#         self.resize(300, 250)
+#         # Name of the window
+#         self.setWindowTitle("PyQt Widget Test")
+
+#         # VBox layout
+#         layout = QVBoxLayout()
+#         self.setLayout(layout)
+
+#         # Widget will start out saying Old Text
+#         self.label = QLabel("Old Text")
+#         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+#         self.label.adjustSize()
+#         layout.addWidget(self.label)
+
+#         # Adds button then goes to update if clicked
+#         button = QPushButton("Update Text")
+#         button.clicked.connect(self.update)
+#         layout.addWidget(button)
+
+#         # Adds another button to print text
+#         button = QPushButton("Print Text")
+#         button.clicked.connect(self.get)
+#         layout.addWidget(button)
+
+#     # Changes the text of the Widget
+#     def update(self):
+#         self.label.setText("New and Updated Text")
+
+#     # Prints current Widget text   
+#     def get(self):
+#         print(self.label.text())
+
+# Creating and Retrieving QLineEdit values
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(300, 250)
+        self.setWindowTitle(" QLineEdit Example")
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        self.input = QLineEdit()
+        self.input.setFixedWidth(150)
+        layout.addWidget(self.input, alignment= Qt.AlignmentFlag.AlignCenter)
+
+        button = QPushButton("Get Text")
+        button.clicked.connect(self.get)
+        layout.addWidget(button)
+
+        button = QPushButton("Clear Text")
+        button.clicked.connect(self.input.clear)
+        layout.addWidget(button)
+
+    def get(self):
+        text = self.input.text()
+        print(text)
 
 # Creating QLabel Widget
 # class Window(QWidget):
@@ -17,49 +80,7 @@ import sys
 #         # Location inside the window, default it (0,0)
 #         label.move(80, 100)
 
-# app = QApplication(sys.argv)
-# Window = Window()
-# Window.show()
-# sys.exit(app.exec())
-
-# How to change current displayed text in QLabel Widget
-class Window(QWidget):
-    def __init__(self):
-        super().__init__()
-        # Window dimensions
-        self.resize(300, 250)
-        # Name of the window
-        self.setWindowTitle("PyQt Widget Test")
-
-        # VBox layout
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-
-        # Widget will start out saying Old Text
-        self.label = QLabel("Old Text")
-        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.label.adjustSize()
-        layout.addWidget(self.label)
-
-        # Adds button then goes to update if clicked
-        button = QPushButton("Update Text")
-        button.clicked.connect(self.update)
-        layout.addWidget(button)
-
-        # Adds another button to print text
-        button = QPushButton("Print Text")
-        button.clicked.connect(self.get)
-        layout.addWidget(button)
-
-    # Changes the text of the Widget
-    def update(self):
-        self.label.setText("New and Updated Text")
-
-    # Prints current Widget text   
-    def get(self):
-        print(self.label.text())
-
 app = QApplication(sys.argv)
-window = Window()
-window.show()
+Window = Window()
+Window.show()
 sys.exit(app.exec())
