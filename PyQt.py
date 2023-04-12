342d8fdd6a312e5dec43788b043d90e50ae7c7ba
zalane2 <72633029+zalane2@users.noreply.github.com>
2023-04-11 21:52:01 -0400

    Now saves csv file and passes into a new menu

--- PyQt.py
+++ PyQt.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import (
     QComboBox, QWidget, QApplication, QMainWindow, QVBoxLayout, QPushButton, QHBoxLayout, QLabel)
-from PyQt6.QtCore import Qt
+from new_menu import *
 import sys
 import os
 
@@ -9,7 +9,9 @@ import os
 class graphWindow(QWidget):
     def __init__(self):
         super().__init__()
+        # Window Title
         self.setWindowTitle("Graphing Window")
+        # Place holder until ready to show stuff
         layout = QHBoxLayout()
         self.label = QLabel("Options")
         layout.addWidget(self.label)
@@ -24,7 +26,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("PyQt Test")
 
         # File path to the csv files
-        file_path = "C:/Users/zacla/OneDrive/Desktop/Capstone/Spark/csv_files"
+        file_path = "Data/processed"
         # Place all in directory into array
         dir_list = os.listdir(file_path)
         
@@ -32,47 +34,59 @@ class MainWindow(QMainWindow):
         # TODO make option for choosing all
         self.combobox = QComboBox()
         self.combobox.addItems(dir_list)
-        self.combobox.activated.connect(self.next_step)
-
-        # No other window is open if None
-        self.w = None
-
         # Enter and Cancel buttons
         self.enter = QPushButton("Enter", self)
-        # If option is picked in combo box, open new window
-        self.enter.clicked.connect(self.open_window)
         self.cancel = QPushButton("Cancel", self)
-        # Closes the Main Window
-        self.cancel.clicked.connect(self.canceled)
+
+        # No other window is open if None.
+        self.w = None
+        # Holds csv value picked from user.
+        self.csv = None
+
+        # HBox layout for the buttons on the Main window.
         h_layout = QHBoxLayout()
         h_layout.addWidget(self.enter)
         h_layout.addWidget(self.cancel)
-
+        # VBox layout for the overall layout of the window.
         layout = QVBoxLayout()
         layout.addWidget(self.combobox)
         layout.addLayout(h_layout)
-
+        # container holds the entire layout and places it in the center.
         container = QWidget()
         container.setLayout(layout)
-
         self.setCentralWidget(container)
-    
-    def next_step(self, index):
-        # Pass this into original menu and start spark
-        selected = self.combobox.itemText(index)
-        print (selected)
-    
+
+        # Once option is selected, the option is saved to self.csv.
+        self.combobox.activated.connect(self.next_step)
+        # If option is picked in combo box, open new window.
+        self.enter.clicked.connect(self.open_window)
+        # Closes the Main Window.
+        self.cancel.clicked.connect(self.canceled)
+
+    # When an option is selected by the user in the combobox
+    # it is then passed to self.csv that will be used again in
+    # the graph window class.
+    def next_step(self):
+        self.csv = self.combobox.currentText()
+
+    # Opens the graph window and passes the chosen csv file into the
+    # new menu setup.    
     def open_window(self):
-        # Main problem would be that there isn't a default option if nothing is chosen
-        # From the combobox 
-        if self.w is None:
-            self.w = graphWindow()
-            self.w.show()
-
-        else:
-            self.w.close()  # Close window.
-            self.w = None
-
+        # Checks if a value has been selected from the combobox
+        if self.csv != None:
+            # If a graph window is not already open
+            if self.w is None:
+                # Pass csv file into new menu setup()
+                setup(self.csv)
+                # display graph window
+                self.w = graphWindow()
+                self.w.show()
+            else:
+                # Closes graph window if already open and sets to None again.
+                self.w.close()
+                self.w = None
+    
+    # If user hits cancel, then close main window.
     def canceled(self):
         sys.exit(app.exit())
     
