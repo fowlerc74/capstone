afbe98383021a8e0c3d967f59a4a0b71bc6dbca3
zalane2 <72633029+zalane2@users.noreply.github.com>
2023-04-13 15:45:48 -0400

    Update new_menu.py

--- new_menu.py
+++ new_menu.py
@@ -10,10 +10,22 @@ def setup(file):
     print("File Path: " + file_path)
     df = ps.read_csv(file_path)
     sdf = df.to_spark()
-    sdf.printSchema()
     return sdf
 
-def get_linear_stuff(sdf):
-    linear_pred, linear_rain_res = linear(sdf)
-    linear_r2 = linear_rain_res.r2
-    return linear_pred, linear_rain_res, linear_r2 
+
+
+def get_linear_plot(sdf):
+    pred, test = linear(sdf)
+    pred.show()
+    test.show()
+    hold_daily = test.select('DailyPrecipitation').toPandas()
+    daily_rain = list(hold_daily['DailyPrecipitation'])
+    hold_prediction = pred.select('prediction').toPandas()
+    daily_prediction = list(hold_prediction['prediction'])
+    
+    i = 0
+    date_arr = []
+    while (len(daily_rain) != i):
+        i += 1
+        date_arr.append(i)
+    return daily_prediction, daily_rain, date_arr
