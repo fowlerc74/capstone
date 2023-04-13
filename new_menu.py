869c6f565a8c031263b01c116e20870ffcdfbac6
zalane2 <72633029+zalane2@users.noreply.github.com>
2023-04-13 12:14:44 -0400

    Haven't checked if linear works

--- new_menu.py
+++ new_menu.py
@@ -1,5 +1,7 @@
 from pyspark.sql import SparkSession
+from new_ML_Part import *
 import pyspark.pandas as ps
+
 
 spark = SparkSession.builder.getOrCreate()
 
@@ -9,4 +11,9 @@ def setup(file):
     df = ps.read_csv(file_path)
     sdf = df.to_spark()
     sdf.printSchema()
-    
+    return sdf
+
+def get_linear_stuff(sdf):
+    linear_pred, linear_rain_res = linear(sdf)
+    linear_r2 = linear_rain_res.r2
+    return linear_pred, linear_rain_res, linear_r2 
