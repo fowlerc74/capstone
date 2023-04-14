01790196c0d228ba6d721cb4fcd654f21a3c6431
zalane2 <72633029+zalane2@users.noreply.github.com>
2023-04-13 20:48:26 -0400

    Added comments

--- new_menu.py
+++ new_menu.py
@@ -2,9 +2,11 @@ from pyspark.sql import SparkSession
 from new_ML_Part import *
 import pyspark.pandas as ps
 
-
+# Starts spark
 spark = SparkSession.builder.getOrCreate()
 
+# Once the user chooses a csv file, it is then passed 
+# into here and used to create a new data frame.
 def setup(file):
     file_path = 'Data/processed/' + file
     print("File Path: " + file_path)
@@ -13,16 +15,21 @@ def setup(file):
     return sdf
 
 
-
+# This calls the linear function and takes the values from there
+# and places them into arrays to pass back to the graph window.
 def get_linear_plot(sdf):
     pred, test = linear(sdf)
+    # Prints the prediction and test values.
     pred.show()
     test.show()
+    # Holds and passes values for the actual daily precipitation to an array 
     hold_daily = test.select('DailyPrecipitation').toPandas()
     daily_rain = list(hold_daily['DailyPrecipitation'])
+    # Holds the prediction for the daily precipitation to an array
     hold_prediction = pred.select('prediction').toPandas()
     daily_prediction = list(hold_prediction['prediction'])
     
+    # Creates an array of how many days are covered 
     i = 0
     date_arr = []
     while (len(daily_rain) != i):
