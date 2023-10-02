326051c23afbe45fa82f11ba3e78e5e712861eb7
zalane2 <72633029+zalane2@users.noreply.github.com>
2023-10-02 10:35:38 -0400

    Made third column symbol to "x"

--- k_means.py
+++ k_means.py
@@ -3,17 +3,23 @@ from pyspark.ml.clustering import KMeans
 from pyspark.ml.evaluation import ClusteringEvaluator
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
+from PyQt6.QtCore import Qt
 import numpy as np
 import pyqtgraph as pg
 
 
 #  Runs K Means Clustering to cluster data points and returns a scatter plot
-def kmeans(sdf, k, column1, column2):
+def kmeans(sdf, k, column1, column2, column3):
     # Drops NULL values
     sdf = sdf.na.drop()
     # Makes a vector that contains the features that K-Means will
     # use to predict which cluster that they should belong to
-    feat_assembler = k_vector_feature_assembler(column1, column2)
+
+    print("RIGHT HERE")
+    print(column1)
+    print(column2)
+    print(column3)
+    feat_assembler = k_vector_feature_assembler(column1, column2, column3)
 
     # A new DataFrame that has the features vector as a new column.
     new_df = feat_assembler.transform(sdf)
@@ -31,7 +37,7 @@ def kmeans(sdf, k, column1, column2):
     sil_output = silhouette_score(new_df)
     # Makes a scatter plot of the features and which cluster they belong to
     graph, sil_graph = setup_graph(
-        output, kmeans_algo.getK(), sil_output, column1, column2
+        output, kmeans_algo.getK(), sil_output, column1, column2, column3
     )
     return graph, sil_graph
 
@@ -55,19 +61,32 @@ def silhouette_score(df):
     return s_score
 
 
-# Creates a vector that contains Daily Precipitation and Daily Average Dry Bulb Temperature
-# columns and names the column "features"
-def k_vector_feature_assembler(column1, column2):
-    vector = VectorAssembler(
-        inputCols=[column1, column2],
-        outputCol="features",
-    )
+# Creates a vector that contains the variables that the user chosen
+# and names the vector "features".
+def k_vector_feature_assembler(column1, column2, column3):
+    print(column1)
+    print(column2)
+    print(column3)
+    if column3 == "None":
+        vector = VectorAssembler(
+            inputCols=[column1, column2],
+            outputCol="features",
+        )
+    # if column2 == "None" and column3 == "None":
+    #     # Display a error message, must have at least 2
+    #     pass
+    else:
+        vector = VectorAssembler(
+            inputCols=[column1, column2, column3],
+            outputCol="features",
+        )
     return vector
 
 
 # Returns the columns to choose from for K-Means.
 def k_columns():
     columns = [
+        "None",
         "DailyAverageDryBulbTemperature",
         "DailyAverageRelativeHumidity",
         "DailyAverageSeaLevelPressure",
@@ -87,7 +106,7 @@ def k_columns():
 
 
 # Creates the scatter plot from based on the K-Means results and returns the graph.
-def setup_graph(dfoutput, n_clusters, sil_output, column1, column2):
+def setup_graph(dfoutput, n_clusters, sil_output, column1, column2, column3):
     # Selecting the prediction column
     pred = dfoutput.select("prediction").collect()
     # Selecting the first chosen column by the user
@@ -115,24 +134,47 @@ def setup_graph(dfoutput, n_clusters, sil_output, column1, column2):
     sil_graph = pg.PlotWidget(background="w")
     # Describing what axis' represent in the graph
     sil_graph.setTitle("K-Means Silhouette Score")
-    sil_graph.setLabel(
-        "left",
-        "Distance between each point in one cluster is to points in the neighboring cluster",
-    )
+    sil_graph.setLabel("left", "Average distance between clusters")
     sil_graph.setLabel("bottom", "# of Clusters")
     sil_graph.plot(range(2, 11), sil_score, pen=pg.mkPen("b", width=3))
 
+    hold = []
+
+    if column3 != "None":
+        col3 = dfoutput.select(column3).collect()
+        col3_arr = np.array(col3).reshape(-1)
+        plot.setLabel("right", str(column3))
+        for i in range(n_clusters):
+            scatter.setBrush(QColor(pg.intColor(i, n_clusters)))
+            scatter.setPen(QColor(pg.intColor(i, n_clusters)))
+            for j, k in zip(col1_arr[pred_arr == i], col3_arr[pred_arr == i]):
+                print("Col1: ", j, ", Col3: ", k)
+                hold.append(
+                    {
+                        "pos": (j, k),
+                        "brush": QColor(pg.intColor(i, n_clusters)),
+                        "symbol": "x",
+                    }
+                )
+                scatter.addPoints(hold)
+
     # Making a hold array that will hold the position for each point
     # and color of each point and adds the points to the scatter plot
-    hold = []
     for i in range(n_clusters):
         print("Cluster: ", i)
         scatter.setBrush(QColor(pg.intColor(i, n_clusters)))
         scatter.setPen(QColor(pg.intColor(i, n_clusters)))
         for j, k in zip(col1_arr[pred_arr == i], col2_arr[pred_arr == i]):
             print("Col1: ", j, ", Col2: ", k)
-            hold.append({"pos": (j, k), "brush": QColor(pg.intColor(i, n_clusters))})
+            hold.append(
+                {
+                    "pos": (j, k),
+                    "brush": QColor(pg.intColor(i, n_clusters)),
+                    "symbol": "o",
+                }
+            )
             scatter.addPoints(hold)
+
     # Adds the scatter plot to the plot widget
     plot.addItem(scatter)
     return plot, sil_graph
