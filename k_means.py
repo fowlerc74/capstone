7c8d7e9152bf3fad0564c6b8bba2fe082967f55a
zalane2 <72633029+zalane2@users.noreply.github.com>
2023-11-06 10:06:09 -0500

    User selects the axes for K-Means

--- k_means.py
+++ k_means.py
@@ -27,18 +27,21 @@ def kmeans(sdf, k, column1, column2, column3):
     # Fits the new DataFrame that has the features column
     kmeans_fit = kmeans_algo.fit(new_df)
     centers = kmeans_fit.clusterCenters()
-    legend_box = legend(column1, column2, column3, centers, k)
     # The output after K-Means has run, should contain
     # a prediction column that will be for the feature column
     output = kmeans_fit.transform(new_df)
+    cluster_centers = clusters(centers, k)
     # Get the silhouette score and pass into setup_graph
     sil_output = silhouette_score(new_df)
-    # Makes a scatter plot of the features and which cluster they belong to
-    graph, sil_graph = setup_graph(
-        output, kmeans_algo.getK(), sil_output, column1, column2, column3, sdf
-    )
 
-    return graph, sil_graph, legend_box
+    sil_graph = setup_sil_graph(sil_output)
+
+    # Makes a scatter plot of the features and which cluster they belong to
+    # graph = setup_graph(
+    #     output, kmeans_algo.getK(), column1, column2, column3, sdf
+    # )
+
+    return output, sil_graph, cluster_centers
 
 
 # Silhouette score measures how close each point in one cluster is to points in the
@@ -110,32 +113,12 @@ def k_columns():
 
 
 # Makes the the variable section hold additional information.
-# Currently displays what the symbols are and displays the cluster centers
-# and the color of what center is which on the graph.
-def legend(column1, column2, column3, centers, n_clusters):
-    legend_box = QVBoxLayout()
+# displays the cluster centers and the color of what center is which on the graph.
+def clusters(centers, n_clusters):
     cluster_center_box = QVBoxLayout()
-    overall_layout = QHBoxLayout()
-
-    legend_title = QLabel("Legend:")
-    legend_box.addWidget(legend_title)
-
-    col1 = "X: " + str(column1)
-    col2 = "O: " + str(column2)
-    col1_label = QLabel(col1)
-    legend_box.addWidget(col1_label)
-    if column3 != "None":
-        col3 = str(column3) + " = *"
-        col3_label = QLabel(col3)
-        legend_box.addWidget(col3_label)
-
-    if column2 != "None":
-        col2 = str(column2) + " = x"
-        col2_label = QLabel(col2)
-        legend_box.addWidget(col2_label)
-
-    center_title = QLabel("Cluster centers:")
+    center_title = QLabel("Cluster Centers: [column1],[column2],[column3]")
     cluster_center_box.addWidget(center_title)
+
     i = 0
     for center in centers:
         string_center = str(center)
@@ -147,28 +130,62 @@ def legend(column1, column2, column3, centers, n_clusters):
         cluster_center_box.addWidget(center_label)
         i += 1
 
-    overall_layout.addLayout(legend_box)
-    overall_layout.addLayout(cluster_center_box)
+    return cluster_center_box
 
-    return overall_layout
+
+def user_select_x(column1, column2, column3):
+    options = [str(column1), str(column2), str(column3)]
+    x_value = QComboBox()
+    x_value.addItems(options)
+    return x_value
+
+
+def user_select_y(column1, column2, column3):
+    options = [str(column1), str(column2), str(column3)]
+    y_value = QComboBox()
+    y_value.addItems(options)
+    return y_value
+
+
+# Creates the silhouette graph that will display the results of what each score
+# is and turn those results into a line graph.
+def setup_sil_graph(sil_output):
+    # Make an array that holds the results from the silhouette score function
+    sil_score = np.array(sil_output)
+
+    # Set up the silhouette graph
+    sil_graph = pg.PlotWidget(background="w")
+    # Describing what axes represents for the graph
+    sil_graph.setTitle("K-Means Silhouette Score")
+    sil_graph.setLabel("left", "Average distance between clusters")
+    sil_graph.setLabel("bottom", "# of Clusters")
+    sil_graph.plot(range(2, 11), sil_score, pen=pg.mkPen("b", width=3))
+
+    return sil_graph
 
 
 # Creates the scatter plot from based on the K-Means results and returns the graph.
-def setup_graph(dfoutput, n_clusters, sil_output, column1, column2, column3, sdf):
+def setup_graph_k(dfoutput, n_clusters, column1, column2, sdf):
     # Selecting the prediction column
     pred = dfoutput.select("prediction").collect()
-    # Selecting the first chosen column by the user
+    # Selecting the first chosen column for the X axis
     col1 = dfoutput.select(column1).collect()
+    # Selecting the second chosen column for the Y axis
+    col2 = dfoutput.select(column2).collect()
 
     # Turns the selected columns into numpy arrays
     col1_arr = np.array(col1).reshape(-1)
+    col2_arr = np.array(col2).reshape(-1)
     pred_arr = np.array(pred).reshape(-1)
-    sil_score = np.array(sil_output)
 
     # Makes a plot widget and setting the background white
     plot = pg.PlotWidget(background="w")
     # Creates a scatter plot and sets hovering to true
-    scatter = pg.ScatterPlotItem(hoverable=True, hoverPen='g')
+    scatter = pg.ScatterPlotItem(hoverable=True, hoverPen="g")
+    # Describing what the axes represent for the scatter plot
+    plot.setLabel("bottom", str(column1))
+    plot.setLabel("left", str(column2))
+    plot.setWindowTitle("K-Means")
 
     # When points are hovered over, print
     def on_hover(evt, points):
@@ -185,73 +202,18 @@ def setup_graph(dfoutput, n_clusters, sil_output, column1, column2, column3, sdf
 
     scatter.sigHovered.connect(on_hover)
 
-    # Describing what axis' represent in the graph
-    plot.setLabel("bottom", "Days")
-    plot.setWindowTitle("K-Means")
-
-    # Set up the silhouette graph
-    sil_graph = pg.PlotWidget(background="w")
-    # Describing what axis' represent in the graph
-    sil_graph.setTitle("K-Means Silhouette Score")
-    sil_graph.setLabel("left", "Average distance between clusters")
-    sil_graph.setLabel("bottom", "# of Clusters")
-    sil_graph.plot(range(2, 11), sil_score, pen=pg.mkPen("b", width=3))
-
-    counter = 0
-
-    date_arr = []
-    hold_daily = dfoutput.select("DATE").collect()
-
-    while len(hold_daily) != counter:
-        counter += 1
-        date_arr.append(counter)
-
-    # Making a hold array that will hold the position for each point
-    # and color of each point and adds the points to the scatter plot
-    hold = []
-
-    if column3 != "None":
-        col3 = dfoutput.select(column3).collect()
-        col3_arr = np.array(col3).reshape(-1)
-        for i in range(n_clusters):
-            scatter.setBrush(QColor(pg.intColor(i, n_clusters)))
-            for j, k in zip(date_arr, col3_arr[pred_arr == i]):
-                hold.append(
-                    {
-                        "pos": (date_arr[j], k),
-                        "brush": QColor(pg.intColor(i, n_clusters)),
-                        "symbol": "star",
-                    }
-                )
-                scatter.addPoints(hold)
-
-    if column2 != "None":
-        col2 = dfoutput.select(column2).collect()
-        col2_arr = np.array(col2).reshape(-1)
-        for i in range(n_clusters):
-            scatter.setBrush(QColor(pg.intColor(i, n_clusters)))
-            for j, k in zip(date_arr, col2_arr[pred_arr == i]):
-                hold.append(
-                    {
-                        "pos": (date_arr[j], k),
-                        "brush": QColor(pg.intColor(i, n_clusters)),
-                        "symbol": "x",
-                    }
-                )
-                scatter.addPoints(hold)
-
+    hold_k_points = []
     for i in range(n_clusters):
         scatter.setBrush(QColor(pg.intColor(i, n_clusters)))
-        for j, k in zip(date_arr, col1_arr[pred_arr == i]):
-            hold.append(
+        for j, k in zip(col1_arr[pred_arr == i], col2_arr[pred_arr == i]):
+            hold_k_points.append(
                 {
-                    "pos": (date_arr[j], k),
+                    "pos": (j, k),
                     "brush": QColor(pg.intColor(i, n_clusters)),
                     "symbol": "o",
                 }
             )
-            scatter.addPoints(hold)
+            scatter.addPoints(hold_k_points)
 
-    # Adds the scatter plot to the plot widget
     plot.addItem(scatter)
-    return plot, sil_graph
+    return plot
